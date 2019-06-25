fe7bf439876e08ad887fd3693a6f1a0c869c74bf
miguelcalado10 <32521738+miguelcalado10@users.noreply.github.com>
2019-06-25 22:47:07 +0100

    Dashboard

--- src/app/pages/private/dashboard/dashboard.page.ts
+++ src/app/pages/private/dashboard/dashboard.page.ts
@@ -1,7 +1,12 @@
 import { File } from './../../../../model/file.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { FilesService } from 'src/app/services/files.service';
+import { Storage } from '@ionic/storage';
+import {Platform} from '@ionic/angular';
+
+import { IonInfiniteScroll } from '@ionic/angular';
+import { NumberSymbol } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,34 +14,100 @@ import { FilesService } from 'src/app/services/files.service';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
-  files : Array<File>;
 
-  constructor( private authService: AuthenticationService, private filesServices: FilesService ) { }
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
+  allFiles: Array<File>;
+  files: Array<File>;
+  screenHeight: number;
+
+// tslint:disable-next-line: max-line-length
+  constructor( private authService: AuthenticationService, private storage: Storage, private filesServices: FilesService, private platform: Platform ) {}
 
   ngOnInit() {
-    // get current token
 
-    let token: string = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJwaXAucC5kZXZlbG9wQGdtYWlsLmNvbSIsImF1ZCI6InBpcHBvcnRhbC5ldSIsImlhdCI6MTU2MTIyMjA1OSwiZGF0YSI6eyJlbWFpbCI6InBjYWxhZG85OUBnbWFpbC5jb20iLCJlbXBsb3llZV9pZCI6IjMzNzQzIn19.uTcTwpxfr4OPRDZXnU3zRg5YwoJojMts6hZwBr4CXsk';
+    // this.platform.ready().then((readySource) => {
+    this.screenHeight = this.platform.height();
+    // });
+    console.log(this.screenHeight);
 
-    this.filesServices.getFiles( token ).subscribe( (res:any) => {
-      console.log("Files subscribe")
-      console.log(res)
-      this.files = new Array<File>();
-      if(res.files) {
-        res.files.forEach(element => {
-          this.files.push({
-            id: element.file_id,
-            name: element.filename,
-            s3_id: element.string,
-            date: element.file_date
-          })
-        });
-      }
-    });
+    this.storage.get('TOKEN_KEY').then( (data) => {
+
+      const token: string = data;
+      // console.log(token);
+
+      this.filesServices.getFiles( token ).subscribe( (res: any) => {
+        console.log("Files subscribe");
+        console.log(res);
+
+        let i = 0;
+
+        this.allFiles = new Array<File>();
+        this.files = new Array<File>();
+
+        if ( res.files ) {
+          res.files.forEach(element => {
+
+            // this.allFiles.push({
+            //   id: element.file_id,
+            //   name: element.filename,
+            //   s3_id: element.string,
+            //   date: element.file_date
+            // });
+
+            // // Apresenta primeiros 8
+            // if( i < 8) {
+              this.files.push({
+                id: element.file_id,
+                name: element.filename,
+                s3_id: element.string,
+                date: element.file_date
+              });
+            // }
+
+            // i++;
+
+          });
+        }
+      });
+
+    })
+    // .catch(error => handleError(error))
+    // .finally(() => handleFinally())
+    ;
+
   }
 
   logout() {
     this.authService.logout();
   }
+
+  // loadData(event) {
+  //   console.log(this.files.length)
+  //   console.log(this.allFiles.length)
+
+  //   setTimeout(() => {
+
+  //     for (let i = 0; i < 8; i++) {
+  //       let current = i + this.files.length;
+
+  //       if( current < this.allFiles.length ) {
+  //         this.files.push(this.allFiles[current]);
+  //       }
+
+  //     }
+  //     event.target.complete();
+
+  //     if (this.files.length === 1000) {
+  //       event.target.disabled = true;
+  //     }
+
+  //   }, 500);
+  // }
+
+
+  // toggleInfiniteScroll() {
+  //   this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+  // }
 
 }
